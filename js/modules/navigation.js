0f79a55365d04c40e675066e074c6370cf717a09
meatbags <jxburrow@gmail.com>
2019-03-23 14:09:33 +1100

    first draft layout

--- js/modules/navigation.js
+++ js/modules/navigation.js
@@ -12,6 +12,22 @@ class Navigation {
         }
       })
     }
+
+    // mobile nav/ menu
+    this.mobileNavButton = document.querySelector('.mobile-nav-button');
+    this.mobileNav = document.querySelector('.mobile-nav');
+    if (this.mobileNavButton && this.mobileNav) {
+      const evtType = this.isMobile ? 'touchend' : 'click';
+      this.mobileNavButton.addEventListener(evtType, evt => {
+        if (!this.mobileNavButton.classList.contains('active')) {
+          this.mobileNavButton.classList.add('active');
+          this.mobileNav.classList.add('active');
+        } else {
+          this.mobileNavButton.classList.remove('active');
+          this.mobileNav.classList.remove('active');
+        }
+      });
+    }
   }
 }
 
