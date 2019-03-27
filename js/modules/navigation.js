1895545265d391848daa24e527aebbaf6d490182
meatbags <jxburrow@gmail.com>
2019-03-27 11:47:22 +1100

    changes #3

--- js/modules/navigation.js
+++ js/modules/navigation.js
@@ -17,8 +17,7 @@ class Navigation {
     this.mobileNavButton = document.querySelector('.mobile-nav-button');
     this.mobileNav = document.querySelector('.mobile-nav');
     if (this.mobileNavButton && this.mobileNav) {
-      const evtType = this.isMobile ? 'touchend' : 'click';
-      this.mobileNavButton.addEventListener(evtType, evt => {
+      this.mobileNavButton.addEventListener('click', evt => {
         if (!this.mobileNavButton.classList.contains('active')) {
           this.mobileNavButton.classList.add('active');
           this.mobileNav.classList.add('active');
@@ -27,6 +26,40 @@ class Navigation {
           this.mobileNav.classList.remove('active');
         }
       });
+    }
+
+    // mobile grid
+    this.grid = document.querySelector('.grid');
+    if (this.grid && this.isMobile) {
+      this.gridItems = this.grid.querySelectorAll('.grid__item');
+      /*
+      window.addEventListener('scroll', evt => {
+        this.onScroll();
+      });
+      this.onScroll();
+      */
+      this.gridItems.forEach(item => {
+        item.addEventListener('touchstart', evt => {
+          if (!item.classList.contains('active')) {
+            this.grid.querySelectorAll('.grid__item.active').forEach(el => { el.classList.remove('active'); });
+            item.classList.add('active');
+          }
+        });
+      })
+    }
+  }
+
+  onScroll() {
+    // highlight active grid item (mobile)
+    for (let i=0, lim=this.gridItems.length; i<lim; ++i) {
+      const rect = this.gridItems[i].getBoundingClientRect();
+      if (!(rect.left == 0 && rect.top == 0) && (rect.top >= 0 && rect.top + rect.height < window.innerHeight)) {
+        if (!this.gridItems[i].classList.contains('active')) {
+          this.grid.querySelectorAll('.grid__item.active').forEach(el => { el.classList.remove('active'); });
+          this.gridItems[i].classList.add('active');
+        }
+        break;
+      }
     }
   }
 }
