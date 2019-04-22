5fa00b1eaf4ba79f28d5e21f8967b173c6181062
Xavier Burrow <jxburrow@gmail.com>
2019-04-22 13:15:25 +1000

    new config options & js autosize

--- js/modules/landing-page.js
+++ js/modules/landing-page.js
@@ -18,16 +18,33 @@ class LandingPage {
       }
       */
 
-      // rotate menus
+      // transform index navigation
       const nav = document.querySelector('.nav--index');
-      const breakpoint = {
-        medium: 1500,
-        tablet: 950,
-        mobilePortrait: 400,
-      };
       if (nav) {
+        const breakpoint = {
+          medium: 1500,
+          tablet: 950,
+          mobilePortrait: 400,
+        };
+        const rinse = (text) => {
+          return text.replace(/[^0-9.-]/g, "");
+        }
         const resize = () => {
-
+          const w = window.innerWidth;
+          const h = window.innerHeight;
+          const x = parseFloat(rinse(w > breakpoint.tablet ? nav.dataset['x'] : nav.dataset['mx']));
+          const y = parseFloat(rinse(w > breakpoint.tablet ? nav.dataset['y'] : nav.dataset['my']));
+          const rot = parseFloat(rinse(w > breakpoint.tablet ? nav.dataset['rot'] : nav.dataset['mrot']));
+          const scale = parseFloat(rinse(w > breakpoint.tablet ? '100.0' : nav.dataset['mscale']));
+          if (!isNaN(x) && !isNaN(y) && !isNaN(rot) && !isNaN(scale)) {
+            const left = w * (x / 100) + 'px';
+            const top = h * (y / 100) + 'px';
+            const s = scale / 100;
+            const transformText = `translate(-50%, -50%) rotate(${rot}deg) scale(${s})`;
+            nav.style.left = left;
+            nav.style.top = top;
+            nav.style.transform = transformText;
+          }
         };
         resize();
         window.addEventListener('resize', () => { resize(); });
